25af457db4d7453aa8a90b2c6db805f9c0eb4c2d
CucNg <cucnguyen@kms-technology.com>
2024-09-21 22:07:20 +0700

    first commit

--- tests/test/Applied_pageObjectModel.spec.ts
+++ tests/test/Applied_pageObjectModel.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "../../fixtures/BasePage";
 const data = JSON.parse(JSON.stringify(require("../../utils1/placeOrderTestData.json")));
 
-test.describe('@Exercise_Apply_POM_Fixtures', () => {
+test.describe('@playwright_exercise @smoke @regression', () => {
   for (const userData of data) {
     test.only(`Test case: Using fixture ${userData.checkoutFirstName}`, async ({
       inventoryPage, cartPage,
